fc9259060c739e964179b549c986fd2ecb66c96e
MariDV <m.vilela33@gmail.com>
2019-04-18 11:44:24 -0700

    new

---
+++ CSharpProjectMariana/OtherVariables.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProjectMariana
+{
+    class OtherVariables
+    {
+        enum weekdays
+        {
+            Monday = 1, Tuesday = 2, Wednesday = 3, Thursday = 4, Friday = 5, Saturday = 6, Sunday = 7 //every item has a index number Monday should be 0, but you can change that information.*/
+        }
+
+        struct dog
+        {
+            public int BirthYear;
+            public string name;
+            public bool isFriendly;
+
+            public void WhoAreYou()
+            {
+                Console.WriteLine($"Hello my name is {name}," + $"I was born at {BirthYear} " + $"people say I am friendly thats {isFriendly}.");
+            }
+        }
+            public static void body()
+            {
+                Console.WriteLine("test for static other variables");
+                Console.WriteLine(weekdays.Monday);
+                Console.WriteLine($"the enum weekdays monday is: {weekdays.Monday.ToString()}");
+                Console.WriteLine((int)weekdays.Monday); //this way you can see the number(index) conected with this object.*/
+                Console.WriteLine($"the enum weekdays monday value is: {(int)weekdays.Monday}");
+                Console.WriteLine("\n\n");
+
+                dog puppy;
+                puppy.BirthYear = 2018;
+                puppy.name = "Aylla";
+                puppy.isFriendly = true;
+                puppy.WhoAreYou();
+                //Console.WriteLine($"Hello my name is {puppy.name}," + $"I was born at {puppy.BirthYear} " + $"people say I am friendly thats {puppy.isFriendly}.");
+                Console.WriteLine("\n\n");
+
+
+                Console.WriteLine("\n");
+                Console.WriteLine("\t this is a tab");
+                Console.WriteLine("/\\/\\");
+                Console.WriteLine("she said \"yes\"");
+                Console.WriteLine("helll\boo");
+                Console.WriteLine("\a");
+                Console.WriteLine("this is the symbol of PI \u03c0");
+                Console.WriteLine("\n");
+
+                string testString = "hellooooo C sharp!       ";
+                Console.WriteLine($"original string is \t{testString}");
+                Console.WriteLine($"testString.Substring(5) \t{testString.Substring(5)}");
+                Console.WriteLine($"testString.Substring(10,7) \t{testString.Substring(10,7)}");
+                Console.WriteLine($"testString.Remove(5) \t{testString.Remove(5)}");
+                Console.WriteLine($"testString.Remove(5,4) \t{testString.Remove(5,4)}");
+                Console.WriteLine($"testString.Remove(11) \t{testString.Remove(11)}#!");
+                Console.WriteLine($"testString.Replace(one,other) \t {testString.Replace("sharp","#!")}");
+                Console.WriteLine($"testString.Replace(one,other) \t {testString.Replace("sharp", "#!").Trim()}");
+                Console.WriteLine($"testString.Replace(one,other) \t {testString.Replace("sharp", "#!").Trim().ToUpper()}");
+                Console.WriteLine($"testString.Replace(one,other) \t {testString.Replace("sharp", "#!").Trim().ToUpper().Remove(5,4).Remove(7,1)}");
+
+
+
+
+
+            Console.ReadLine(); //you need this to see the value when pressing start
+
+        }
+    }
+}
+
--- CSharpProjectMariana/Program.cs
+++ CSharpProjectMariana/Program.cs
@@ -8,95 +8,11 @@ namespace CSharpProjectMariana
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            //this is single line comment
-            /*
-             this is multi line comment
-             boolean value is true or false
-             you have to initialize boolean value before using it
-             */
-            bool booleanVar = false;
-            Console.WriteLine("the booleanVar value is either {0} or {1}", booleanVar, !booleanVar);
-            Console.WriteLine("the booleanVar value is either " + booleanVar + "or " + !booleanVar);
-            Console.WriteLine($"the booleanVar value is either {booleanVar} or {!booleanVar}");
-            Console.WriteLine($"the size of the boolean is {sizeof(bool)} byte");
-            Console.WriteLine($"\n");
-
-            byte bytevar = 1;
-            Console.WriteLine($"the byteVar value is {bytevar}");
-            Console.WriteLine($"the max value for byte is: {byte.MaxValue}");
-            Console.WriteLine($"the max value for byte is: {byte.MinValue}");
-            Console.WriteLine($"the size of byte is {sizeof(byte)} byte");
-            Console.WriteLine($"\n");
-
-            sbyte sbytevar = 2;
-            Console.WriteLine($"the sbyteVar value is {sbytevar}");
-            Console.WriteLine($"the max value for sbyte is: {sbyte.MaxValue}");
-            Console.WriteLine($"the max value for sbyte is: {sbyte.MinValue}");
-            Console.WriteLine($"the size of sbyte is {sizeof(sbyte)} bytes");
-            Console.WriteLine($"\n");
-
-            char charvar = 'S';
-            Console.WriteLine($"the charVar value is {charvar}");
-            Console.WriteLine($"the size of char is {sizeof(char)} bytes");
-            Console.WriteLine($"\n");
-
-            char charVarUnide = '\u01A9';
-            Console.WriteLine($"The charVarUnide value is {charVarUnide}");
-            Console.WriteLine($"\n");
-
-            decimal decimalVar = 15_415_498_714m;
-            Console.WriteLine($"the decimalVar value is {decimalVar}");
-            Console.WriteLine("formatted decimalVar is: "+String.Format("{0:n0}", decimalVar));
-            Console.WriteLine($"the max value for decimal is: {decimal.MaxValue}");
-            Console.WriteLine($"the max value for decimal is: {decimal.MinValue}");
-            Console.WriteLine($"the size of decimal is {sizeof(decimal)} bytes");
-            Console.WriteLine($"\n");
-
-            double doubleVar = 3.75d;
-            Console.WriteLine($"the doubleVar value is {doubleVar}");
-            Console.WriteLine($"the max value for double is: {double.MaxValue}");
-            Console.WriteLine($"the max value for double is: {double.MinValue}");
-            Console.WriteLine($"the size of double is {sizeof(double)} bytes");
-            Console.WriteLine($"\n");
-
-            float floatVar = 2.99f;
-            Console.WriteLine($"the floatVar value is {doubleVar}");
-            Console.WriteLine($"the max value for float is: {float.MaxValue}");
-            Console.WriteLine($"the max value for float is: {float.MinValue}");
-            Console.WriteLine($"the size of float is {sizeof(float)} bytes");
-            Console.WriteLine($"\n");
-
-            int intvar = -12345;
-            Console.WriteLine($"the intvar value is {intvar}");
-            Console.WriteLine($"the max value for intvar is: {string.Format ("{0:no}", int.MaxValue)}");
-            Console.WriteLine($"the max value for intvar is: {string.Format("{0:no}", int.MinValue)}");
-            Console.WriteLine($"the size of intvar is {sizeof(int)} bytes");
-            Console.WriteLine($"\n");
-
-            uint uintvar = 12345;
-            Console.WriteLine($"the uintvar value is {uintvar}");
-            Console.WriteLine($"the max value for uintvar is: {string.Format("{0:no}", uint.MaxValue)}");
-            Console.WriteLine($"the max value for uintvar is: {string.Format("{0:no}", uint.MinValue)}");
-            Console.WriteLine($"the size of uintvar is {sizeof(uint)} bytes");
-            Console.WriteLine($"\n");
-
-            int intbinaryVar = 0b11_0110_1010_1010_1010_0001;
-            int hexadecimalVar = 0x00ff_05ff;
-            Console.WriteLine($"the intbinaryVar value is {intbinaryVar}");
-            Console.WriteLine($"the hexadecimalVar value is {hexadecimalVar}");
-            Console.WriteLine($"\n");
-
-
-            long logvar = 1234567891234567l;
-            Console.WriteLine($"the longvar value is {longvar}");
-            Console.WriteLine($"the max value for longvar is: {string.Format("{0:no}", long.MaxValue)}");
-            Console.WriteLine($"the max value for longvar is: {string.Format("{0:no}", long.MinValue)}");
-            Console.WriteLine($"the size of long is {sizeof(long)} bytes");
-            Console.WriteLine($"\n");
-
-            Console.ReadLine();
+            // Variables.body(); // what you call here will appear when you press start, now I'm calling class variable, but if I whant other variables I must call OtherVariables.body()*/
+            OtherVariables.body();
         }
     }
 }
