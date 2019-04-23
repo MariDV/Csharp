3de5259e14f7c72786457bd1498122d6e76abc7a
MariDV <m.vilela33@gmail.com>
2019-04-23 12:49:49 -0700

    games

--- CSharpProjectMariana/ConditionalStatements.cs
+++ CSharpProjectMariana/ConditionalStatements.cs
@@ -17,21 +17,66 @@ namespace CSharpProjectMariana
 
             int resposta;
             bool itsok = int.TryParse(userInput, out resposta);
-
+            int thisYear = (DateTime.Now).Year;
             if (itsok)
             {
-                int datauser = int.Parse(userInput);
-                int useryear = 2019 - datauser;
-                Console.WriteLine($"You typed {datauser}");
-                Console.WriteLine($"You were born at" + useryear);
-            }
-            else
+                if (resposta < 0)
+                {
+                    Console.WriteLine("stop this, no negative numbers");
+                }
+                else
+                {
+                    if (resposta > 100)
+                    {
+                        Console.WriteLine("Dude, you are probably dead..");
+                    }
+                    Console.WriteLine($"You were born at {thisYear - resposta}");
+                }
+            } else
             {
                 Console.WriteLine($"THIS IS NOT A NUMBER mothafoca");
             }
+
+            //------------_____________________-------------------___________________-----------------------___________________________
+
+            //and operator &&
+            //or operator ||
+
+            //exemplos de uso && , ||
+              
+            string weather = "rainy";
+            bool haveUmbrella = true;
+            if (weather == "rainy" && haveUmbrella)
+            {
+                Console.WriteLine("You are safe and dry");
+            }
+            if (weather == "rainy" && !haveUmbrella)
+            {
+                Console.WriteLine("Ops, you will get wet as hell");
+            }
+
+            string program1 = "general programming";
+            string program2 = "e-commerce";
+            Console.WriteLine("what is your program?");
+            string userprogram = Console.ReadLine();
+
+            if(userprogram == program1 || userprogram == program2)
+            {
+                Console.WriteLine("you are in IT department");
+
+            }
+            else
+            {
+                Console.WriteLine("you are not in IT department");
+            }
+
             
-            
-            //Console.ReadLine();
+
+
+
+
+
+            Console.ReadLine();
 }
     }
 }
---
+++ CSharpProjectMariana/randomNum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProjectMariana
+{
+    class randomNum
+    {
+       public static void body()
+        {
+            Random qualquercoisa = new Random();
+            int blabla = qualquercoisa.Next(1, 10);
+            Console.WriteLine("Guess a number between 1-10:");
+            string userguess;
+            userguess = Console.ReadLine();
+            int resposta;
+            bool allgood = int.TryParse(userguess, out resposta);
+            Console.WriteLine("the invisible number is: " + blabla);
+
+            if (allgood)
+            {
+                if (resposta > blabla)
+                {
+                    Console.WriteLine("It's less than that");
+                }
+                if (resposta < blabla)
+                {
+                    Console.WriteLine("It's greater than that");
+                }
+                if (resposta == blabla)
+                {
+                    Console.WriteLine("Congrats you are correct!!!");
+                }
+                else
+                {
+                    Console.WriteLine("please insert a number");
+                }
+            }
+           
+            Console.ReadLine();
+
+        }
+
+    }
+}
