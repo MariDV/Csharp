b74049b99568dea3dbb7bd9ae752360edec5ffa7
MariDV <m.vilela33@gmail.com>
2019-04-30 12:17:57 -0700

    functions

--- CSharpProjectMariana/ArraysLibrary.cs
+++ CSharpProjectMariana/ArraysLibrary.cs
@@ -11,17 +11,17 @@ namespace CSharpProjectMariana
         public static void body()
         {
             float[,] floarArr = new float[2, 5];
-            for (int i = 0; i < 2; i += 1)
+            for (int i = 0; i < floarArr.GetLength(0); i += 1)
             {
-                for (int j = 0; j < 5; j += 1)
+                for (int j = 0; j < floarArr.GetLength(1); j += 1)
                 {
-                    floarArr[i, j] = (i*5) + j+1;
+                    floarArr[i, j] = (i* floarArr.GetLength(1)) + j+1;
                 }
             }
 
-            for (int i = 0; i < 2; i += 1)
+            for (int i = 0; i < floarArr.GetLength(0); i += 1)
             {
-                for (int j = 0; j < 5; j += 1)
+                for (int j = 0; j < floarArr.GetLength(1); j += 1)
                 {
                     Console.WriteLine($"floatArr[{i},{j}]={floarArr[i, j]}");
                     Console.ReadLine();
