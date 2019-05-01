377e4aa5588177c5fac7a3833da6aa109843c278
MariDV <m.vilela33@gmail.com>
2019-05-01 09:52:06 -0700

    jagged Array

---
+++ CSharpProjectMariana/Array-lib.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProjectMariana
+{
+    class Array_lib
+    {
+        public static void body()
+        {
+            if (true)
+            {
+                int[][] jaggedArr = new int[3][];
+                jaggedArr[0] = new int[2] { 1, 2 };
+                jaggedArr[1]= new int[4] { 345, 34, 6, 3};
+                jaggedArr[2]= new int[5] { 434, 323, 767, 9898, 111};
+            }
+
+            string[][] jaggedStringArr = new string[4][] {
+                new string[] {"Pao"},
+                new string[] { "Tapioca", "Pacoca", "Acai", "Carambola", "Caja"},
+                new string[] { "Couve", "Ovos", "Frango" },
+                new string[] { "Whey", "Cocada" }
+            };
+            //to print the index 3 array, all elements one by one
+            for (int j = 0; j < jaggedStringArr.Length; j++)
+            {
+
+                for (int i = 0; i < jaggedStringArr[j].Length; i++)
+                {
+                    Console.WriteLine(jaggedStringArr[j][i] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //acces to the jagged array element
+            Console.Write("the value of jaggedStringArr[3][1] : ");
+            Console.WriteLine(jaggedStringArr[3][1]);//will print the second element from the 3rd array
+
+
+            double[][,] twoDjaggedArray = new double[4][,]
+            {
+                new double[2,5] {{2.1d,5.3d,7.1d,9.2d,7.5d},{2.8d,5.2d,7.6d,9.9d,7.2d}},
+                new double[1,5] {{5.2d,2.5d,6.7d,2.3d,7.8d}},
+                new double[2,6] {{1.1d,3.7d,5.6d,9.8d,9.9d,2.3d},{4.4d,0.3d,5.5d,7.2d,1.9d,3.5d}},
+                new double[2,5] {{2.2d,3.3d,4.4d,5.5d,6.6d},{2.1d,3.2d,4.3d,5.4d,6.5d}}
+            };
+
+            Console.ReadLine();
+        }
+    }
+}
--- CSharpProjectMariana/functions.cs
+++ CSharpProjectMariana/functions.cs
@@ -8,6 +8,169 @@ namespace CSharpProjectMariana
 {
     class functions
     {
-        
+        public static void body(){
+
+        greetings("saygin", ConsoleColor.Green);
+        greetings("siamak", ConsoleColor.Yellow);
+        greetings("sina", ConsoleColor.DarkMagenta);
+        greetings("jose", ConsoleColor.Blue);
+        greetings("mariana", ConsoleColor.DarkRed);
+
+
+        //second function
+
+        saywelcome("Jose", "ITD Canada");
+        saywelcome("Mariana", "ITD Canada");
+
+
+        //my functions
+        Console.WriteLine($"The result for add is" + add(5,10));
+            Console.WriteLine($"The result for multiply is" + multiply(5,10));
+            Console.WriteLine($"The result for add subtract is" +subtract(5,10));
+            Console.WriteLine($"The result for divide is" + divide(5,10));
+            Console.ForegroundColor = ConsoleColor.White;
+             add(5,10);
+             multiply(5, 10);
+             subtract(5, 10);
+             divide(5, 10);
+
+         Console.WriteLine(multiply(add(4,5),divide(20,10)));
+         Console.ReadLine();
+        //}
+
+
+
+    //-first function-
+    //this is how I can explain what Im doing in my function
+    /// <summary>
+    /// This function asks for two parameters, 1st the name (must have), sencond one you can decide to use a default color.
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="color"></param>
+    void greetings(string name, ConsoleColor color = ConsoleColor.White)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine($"hello my name is {name}");
+    }
+
+
+    void saywelcome(string name, string school = "ITD")
+    {
+        Console.WriteLine($"hello {name}, welcome to {school}");
+    }
+
+    double add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    //if you don't know how many numbers you can do it like this:
+    /// <summary>
+    /// create a params list, give a name, in this case args
+    /// create a new variable (double), give a value
+    /// say what is the operation
+    /// ask to return
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    double add9(params double[] args)
+    {
+        double sum = 0;
+        for (int i = 0; i < args.Length; i++)
+        {
+            sum += args[i];
+        }
+        return sum;
+    }
+    double add2(params double[] args)
+    {
+        StringBuilder sb = new StringBuilder();
+        double sum = 0;
+        for (int i = 0; i < args.Length; i++)
+        {
+            sb.Append(args[i].ToString());
+            if (i == args.Length - 1) sb.Append(" ");
+            else sb.Append(" + ");
+            sum += args[i];
+        }
+        sb.Append("= ");
+        sb.Append(sum.ToString());
+        Console.WriteLine(sb.ToString());
+        return sum;
+    }
+    double multiply(double num1, double num2)
+    {
+        return num1 * num2;
+    }
+    double subtract(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+    double divide(double num1, double num2)
+    {
+        return num1 / num2;
+    }
+
+    /*void numtowords(int values)
+    {
+        int _values = Convert.ToInt32(values);
+        String name = " ";
+        switch (values)
+        {
+
+            case 1:
+                name = "One";
+                break;
+            case 2:
+                name = "Two";
+                break;
+            case 3:
+                name = "Three";
+                break;
+            case 4:
+                name = "Four";
+                break;
+            case 5:
+                name = "Five";
+                break;
+            case 6:
+                name = "Six";
+                break;
+        }
+        return;
     }
 }
+//public static int countDigit(int values)
+{
+    if (values < 0)
+    {
+
+    }
+}
+//public static int reverse(int values)
+{
+    int reverse = 0;
+    int lastDigit;
+    int original = values;
+    if (values < 0)
+    {
+        values *= -1;
+    }
+
+    while (values > 0)
+    {
+        lastDigit = values % 10;
+        reverse *= 10;
+        reverse += lastDigit;
+        values /= 10;
+    }
+    if (original > 0)
+    {
+
+    }
+}*/
+
+    }
+}            
+}
+  
